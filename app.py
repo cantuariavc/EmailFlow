54d9f083aff841c780f96038f4af35deaa8a28bd
Vinícius Cantuária <vinicius.cantuaria@zerum.com>
2025-09-10 14:39:08 -0300

    Implementa interface do usuário

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.get("/")
 def index():
-    return 'Index Page'
+    return render_template("index.html")
